6c6ffb08f16a0f268987b76b6af59e075864743c
David Cao <david@cao.sh>
2020-04-27 16:46:56 -0700

    Update landing text

--- src/components/nav.js
+++ src/components/nav.js
@@ -30,7 +30,7 @@ const Nav = () => (
           <Link to="/events" className="transition duration-100 inline-block border-b border-transparent hover:border-gray-100">Events</Link>
         </li>
         <li class="text-2xl mt-1 text-gray-100">
-          <Link to="/class" className="transition duration-100 inline-block border-b border-transparent hover:border-gray-100">Class</Link>
+          <Link to="/contact" className="transition duration-100 inline-block border-b border-transparent hover:border-gray-100">Contact</Link>
         </li>
       </ul>
       <footer>
@@ -39,7 +39,6 @@ const Nav = () => (
             <li class="mt-1"><a className="transition duration-100 text-gray-500 hover:text-gray-100" href="https://www.instagram.com/ucsdwushu/">Instagram</a></li>
             <li class="mt-1"><a className="transition duration-100 text-gray-500 hover:text-gray-100" href="https://www.facebook.com/UCSDwushu">Facebook</a></li>
             <li class="mt-1"><a className="transition duration-100 text-gray-500 hover:text-gray-100" href="https://www.youtube.com/channel/UCJJOaO7Kk3-u-qHxsRbrFuw">YouTube</a></li>
-            <li class="mt-1"><Link className="transition duration-100 text-gray-500 hover:text-gray-100" to="/contact">Contact</Link></li>
           </ul>
         </div>
       </footer>
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ const IndexPage = () => (
     <SEO title="Home" />
     <div className="px-16 pt-16 min-h-screen flex pb-20">
       <h1 className="text-5xl mt-auto pr-64" style={{ fontVariationSettings: "'wdth' 105" }}>
-        We skate fast and eat ass.
+        We swing swords and do backflips for fun. We are performers and competitors. And sometimes, we sell egg rolls.
         <br />
         This is<span style={{ fontVariationSettings: "'wdth' 125, 'wght' 700" }}> UCSD Wushu.</span>
       </h1>
