21ef7306f3adeeebd37387121b5575e46f68f3e9
David Cao <david@cao.sh>
2020-05-01 00:09:27 -0700

    Add to landing page (temp)

--- src/components/nav.js
+++ src/components/nav.js
@@ -4,7 +4,7 @@ import logo from "../images/ucsd-wushu-logo-white.svg"
 
 const Nav = () => (
   <header>
-    <div className="flex flex-col min-h-screen pt-12 pb-24 px-16">
+    <div className="md:flex flex-col min-h-screen md:pt-12 md:pb-24 md:px-16 py-6 px-8">
       <div>
         <Link
           to="/"
@@ -17,28 +17,28 @@ const Nav = () => (
         </Link>
       </div>
       <ul className="flex-grow mt-16">
-        <li class="text-2xl mt-1 text-gray-100">
+        <li className="text-2xl mt-1 text-gray-100">
           <Link to="/class" className="transition duration-100 inline-block border-b border-transparent hover:border-gray-100">Class</Link>
         </li>
-        <li class="text-2xl mt-1 text-gray-100">
+        <li className="text-2xl mt-1 text-gray-100">
           <Link to="/team" className="transition duration-100 inline-block border-b border-transparent hover:border-gray-100">Team</Link>
         </li>
-        <li class="text-2xl mt-1 text-gray-100">
+        <li className="text-2xl mt-1 text-gray-100">
           <Link to="/demos" className="transition duration-100 inline-block border-b border-transparent hover:border-gray-100">Demos</Link>
         </li>
-        <li class="text-2xl mt-1 text-gray-100">
+        <li className="text-2xl mt-1 text-gray-100">
           <Link to="/events" className="transition duration-100 inline-block border-b border-transparent hover:border-gray-100">Events</Link>
         </li>
-        <li class="text-2xl mt-1 text-gray-100">
+        <li className="text-2xl mt-1 text-gray-100">
           <Link to="/contact" className="transition duration-100 inline-block border-b border-transparent hover:border-gray-100">Contact</Link>
         </li>
       </ul>
-      <footer>
+      <footer className="mt-8">
         <div>
           <ul>
-            <li class="mt-1"><a className="transition duration-100 text-gray-500 hover:text-gray-100" href="https://www.instagram.com/ucsdwushu/">Instagram</a></li>
-            <li class="mt-1"><a className="transition duration-100 text-gray-500 hover:text-gray-100" href="https://www.facebook.com/UCSDwushu">Facebook</a></li>
-            <li class="mt-1"><a className="transition duration-100 text-gray-500 hover:text-gray-100" href="https://www.youtube.com/channel/UCJJOaO7Kk3-u-qHxsRbrFuw">YouTube</a></li>
+            <li className="mt-1"><a className="transition duration-100 text-gray-500 hover:text-gray-100" href="https://www.instagram.com/ucsdwushu/">Instagram</a></li>
+            <li className="mt-1"><a className="transition duration-100 text-gray-500 hover:text-gray-100" href="https://www.facebook.com/UCSDwushu">Facebook</a></li>
+            <li className="mt-1"><a className="transition duration-100 text-gray-500 hover:text-gray-100" href="https://www.youtube.com/channel/UCJJOaO7Kk3-u-qHxsRbrFuw">YouTube</a></li>
           </ul>
         </div>
       </footer>
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,16 +7,21 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <div className="px-16 pt-16 min-h-screen flex pb-20" style={{ paddingLeft: "20rem" }}>
-      <h1 className="text-5xl mt-auto pr-64" style={{ fontVariationSettings: "'wdth' 105" }}>
+      <h1 className="text-4xl mt-auto pr-64" style={{ fontVariationSettings: "'wdth' 105" }}>
         We swing swords and do backflips for fun. We are performers and competitors. And sometimes, we sell egg rolls.
         <br />
         This is<span style={{ fontVariationSettings: "'wdth' 125, 'wght' 700" }}> UCSD Wushu.</span>
       </h1>
     </div>
-    <div className="px-16 pt-16 min-h-screen flex pb-20 bg-black z-0" style={{ paddingLeft: "20rem" }}>
-      <h1 className="text-5xl mt-auto pr-64" style={{ fontVariationSettings: "'wdth' 105" }}>
-        This is some test text, other stuff will go here eventually
-      </h1>
+    <div className="border-solid border-t min-h-screen flex bg-black z-0" style={{ paddingLeft: "20rem" }}>
+      <div className="px-16 pt-16 pb-20 faq-bg border-solid border-l min-w-full">
+        <div className="border-solid border-2 bg-black w-2/3">
+          <h1 className="text-5xl m-8 mb-0 mr-32" style={{ fontVariationSettings: "'wdth' 125, 'wght' 700" }}>
+            <span className="align-text-top text-2xl">01</span> What is wushu?
+          </h1>
+          <p className="m-8 mt-0 mr-32">UCSD practices contemporary Chinese wushu, an exhibition martial art developed after 1949 in the People’s Republic of China based on traditional Chinese martial arts. Wushu performances and competition are based on routines, called forms (taolu), in various events representing different styles and types of weapons used in traditional Chinese martial arts. In addition, there is also sparring (sanda), although the club does not practice sparring.</p>
+        </div>
+      </div>
     </div>
   </Layout>
 )
