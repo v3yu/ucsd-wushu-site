642c82cd089618d4f8ecd748284f20d9100076d6
David Cao <david@cao.sh>
2021-09-09 11:38:03 -0700

    new updates 2021-22

--- src/pages/cabinet.js
+++ src/pages/cabinet.js
@@ -104,7 +104,7 @@ const IndexPage = () => {
         style={{ columns: "3 400px", columnGap: "0.75rem", paddingTop: "65vh", backgroundAttachment: "fixed", backgroundColor: "rgba(0,0,0,.75)", backgroundBlendMode: "multiply" }}
         fluid={data.all.childImageSharp.fluid}>
         <div className="inline-block text-xl lg:text-3xl mb-8" style={{ fontVariationSettings: "'wdth' 125, 'wght' 400" }}>
-          <h1>Everything that isn't rec classes falls under the responsibility of <strong>cabinet</strong>. If you have any questions or concerns, talk to us! We don't bite :)</h1>
+          <h1>Everything that isn't rec classes falls under the responsibility of <strong>cabinet</strong>. If you have any questions or concerns, talk to us—we don't bite!</h1>
         </div>
         <Member
           title="Coach"
@@ -112,55 +112,30 @@ const IndexPage = () => {
           desc="theres a homeless-looking dude who broke into the rimac to use their showers but somehow got lost and now spends his leisure time yelling at kids that their thumbs need to be tucked"
           img={data.bobby.childImageSharp.fluid} />
         <Member
-          title="President"
-          name="Yong-Yi Hu"
-          desc="As a 4th-year with the enthusiasm of a 1st-year, the burnout of a grad student, and the sleep schedule of someone who should've died long ago, this international business and sociocultural anthropology double major who doesn't know what she's doing with her life is trying her best to make UCSD Wushu as awesome as it deserves to be. In her free time, she enjoys standing on her toes, whacking herself in the head with weapons she's not good enough to use, and taking blurry pics that will probably become memes. One of her few skills is listening, however, so if you've got anything to say, she'll be there for you. Just don't ask her to solve any math/science/tech problems. Also, know that if you ride in the car she's driving, your life is in danger. You have been warned."
-          img={data.yy.childImageSharp.fluid} />
-        <Member
-          title="Vice President, External"
-          name="Kaitlyn Trinh"
-          desc="somehow a decade’s experience has not taught her the difference between a bow stance and a drop stance"
-          img={data.kaitlyn.childImageSharp.fluid} />
-        <Member
-          title="Vice President, Internal"
+          title="Co-President"
           name="May Ho"
-          desc="May Ho is a third year student in ERC college. Her major is Chemistry. She started wushu at UCSD Wushu because she wanted to learn a new style of martial arts."
+          desc="May Ho is a fourth year student in ERC college. Her major is Chemistry. She started wushu at UCSD Wushu because she wanted to learn a new style of martial arts."
           img={data.may.childImageSharp.fluid} />
+        <Member
+          title="Co-President, Treasurer"
+          name="Terence Liu"
+          desc="Terence is a fourth year Human Biology major and also the inventory manager of the UCSD Wushu Club. He became interested in martial arts when he watched Jet Li in “Once Upon a Time in China,” and has been a fan since childhood. Despite being in wushu for about a two years, Terence aims to improve himself. He has a sweet tooth and is a Nintendo fan. Also, swords. He likes swords ‘cuz they’re cool."
+          img={data.terence.childImageSharp.fluid} />
         <Member
           title="Secretary"
           name="Felicity Le"
-          desc="Felicity Le is a second year Global Health major in Warren College. She joined wushu when she started college and has enjoyed it ever since. If she’s not at wushu, she can be found at dance practice! She has been dancing her whole life and could not imagine life without it. Along with this, Felicity can commonly be seen surrounded by green things and avocados - both loves of her life."
+          desc="Felicity Le is a third year Global Health major in Warren College. She joined wushu when she started college and has enjoyed it ever since. If she’s not at wushu, she can be found at dance practice! She has been dancing her whole life and could not imagine life without it. Along with this, Felicity can commonly be seen surrounded by green things and avocados - both loves of her life."
           img={data.felicity.childImageSharp.fluid} />
         <Member
-          title="Treasurer"
-          name="Sidney Huen"
-          desc="Sidney joined wushu his freshman year, and some of his hobbies outside of this include watching anime, playing video games, and crying himself to sleep over studying mechanical engineering."
-          img={data.sidney.childImageSharp.fluid} />
-        <Member
-          title="Historian"
-          name="Brian Cheng"
-          desc="Brian loves making and watching action movies!"
-          img={data.brian.childImageSharp.fluid} />
-        <Member
-          title="Demos"
+          title="Demo Coordinator"
           name="David Cao"
-          desc="do you think god stays in heaven because he, too, lives in fear of what he's created here on earth? - spy kids 2"
+          desc="David is a third year student at Sixth majoring in Computer Science and minoring in Political Science and . He started wushu in elementary school and has been practicing ever since. "
           img={data.david.childImageSharp.fluid} />
-        <Member
-          title="Social Chair"
-          name="Sabrina Pham-Vu"
-          desc="Sabrina is a fourth year student, and started wushu two years ago when she came to UCSD. She was born in the year of the cat. Therefore, like most cats, she ignores responsibilities and didn't fill out this bio (don't worry, socials will not be affected by this irresponsiblity, just her grades)."
-          img={data.sabrina.childImageSharp.fluid} />
         <Member
           title="Public Relations"
           name="Kelly Tran"
-          desc="Kelly Tran is a third year student at Sixth College majoring in Speculative Design and minoring in Design and Entrepreneurship. As an interdisciplinary, computing art student she has interest in all mediums of art especially photography."
+          desc="Kelly Tran is a fourth year student at Sixth College majoring in Speculative Design and minoring in Design and Entrepreneurship. As an interdisciplinary, computing art student she has interest in all mediums of art especially photography."
           img={data.kelly.childImageSharp.fluid} />
-        <Member
-          title="Inventory Manager"
-          name="Terence Liu"
-          desc="Terence is a third year Human Biology major and also the inventory manager of the UCSD Wushu Club. He became interested in martial arts when he watched Jet Li in “Once Upon a Time in China,” and has been a fan since childhood. Despite being in wushu for about a year, Terence aims to improve himself. He has a sweet tooth and is a Nintendo fan. Also, swords. He likes swords ‘cuz they’re cool."
-          img={data.terence.childImageSharp.fluid} />
       </BackgroundImage>
     </Layout>
   )
--- src/pages/class.js
+++ src/pages/class.js
@@ -27,29 +27,22 @@ const IndexPage = () => {
             <h1 className="text-3xl lg:text-4xl mt-auto lg:pr-64 z-10" style={{ fontVariationSettings: "'wdth' 125, 'wght' 700" }}>
               Return to Wushu
             </h1>
-            <h3 className="text-lg lg:text-xl text-gray-500" >Last updated <strong>January 5, 2021</strong></h3>
+              <h3 className="text-lg lg:text-xl text-gray-500" >Last updated <strong>September 9, 2021</strong></h3>
           </BackgroundImage>
           <div className="px-8 lg:px-16 py-16">
             <p className="text-lg max-w-3xl">
-              Due to the uncertainty surrounding the rapidly evolving pandemic situation, our approach to classes will change and adapt throughout the year.
-              Please check this website and our social media channels for updates on classes and events.
+                For the 2021-22 school year, UCSD Wushu plans on offering wushu classes primarily in-person. As circumstances evolve, this page will be updated with new information on wushu classes.
             </p>
             <h2 className="text-2xl pt-8 pb-4" style={{ fontVariationSettings: "'wdth' 125, 'wght' 700" }}>Membership</h2>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-2 max-w-5xl">
               <div className="border-solid py-6 pl-6 pr-8" style={{ borderWidth: "1px" }}>
-                <h1 className="text-xl pb-2" style={{ fontVariationSettings: "'wdth' 125, 'wght' 600" }}>In-Person Membership</h1>
-                <p className="pb-4 text-gray-400">This is for if you want to attend both in-person and virtual classes.</p>
-                <p className="text-xl">$40</p>
-              </div>
-              <div className="border-solid py-6 pl-6 pr-8" style={{ borderWidth: "1px" }}>
-                <h1 className="text-xl pb-2" style={{ fontVariationSettings: "'wdth' 125, 'wght' 600" }}>Virtual Membership</h1>
-                <p className="pb-4 text-gray-400">This membership is for if you only want to attend classes virtually.</p>
-                <p className="text-xl">$20</p>
+                <h1 className="text-xl pb-2" style={{ fontVariationSettings: "'wdth' 125, 'wght' 600" }}>Non-Rec Membership</h1>
+                <p className="pb-4 text-gray-400">This membership is required if you plan on attending <strong>non-rec</strong> classes.</p>
+                <p className="text-xl">TBD</p>
               </div>
             </div>
             <p className="text-lg max-w-3xl pt-6">
-              In order to join UCSD Wushu, you must pay a membership fee to join.
-              The fees for each type of membership are listed above.
+              Wushu classes are divided into two categories: <strong>rec</strong> classes are offered through UCSD Recreation; as a result, payment is handled through the UCSD Recreation website. On the other hand, <strong>non-rec</strong> classes are arranged directly with UCSD Wushu; as a result, we handle payment for these classes. This rec/non-rec divide helps us fund reasonable coaching compensation and other club activities.
             </p>
             <p className="text-lg max-w-3xl pt-3">
               We accept payment in the following forms:
@@ -64,30 +57,20 @@ const IndexPage = () => {
               <div className="border-solid py-6 pl-6 pr-8" style={{ borderWidth: "1px" }}>
                 <h1 className="text-xl pb-2" style={{ fontVariationSettings: "'wdth' 125, 'wght' 600" }}>Fundamentals Training</h1>
                 <p className="pb-6 text-gray-400">This class will focus on training fundamental techniques to build mastery of the basics. Beginner-friendly, but advanced students welcome!</p>
-                <p className="text-lg">Mon 10-10:45a, <a href="https://goo.gl/maps/MxYY4BcATyLVYx1B9" className="border-b border-gray-700 hover:border-gray-100">Muir Field</a> | <a href="https://rec.ucsd.edu/Program/GetProgramDetails?courseId=f89da2fe-4ab6-4112-806b-57352f1b1bbf&semesterId=7ccf6749-da82-48b0-bfa5-7551fdbcc57f" className="border-b border-gray-700 hover:border-gray-100">Register on rec.ucsd.edu</a></p>
+                <p className="text-lg">Time and Location TBD</p>
               </div>
               <div className="border-solid py-6 pl-6 pr-8" style={{ borderWidth: "1px" }}>
                 <h1 className="text-xl pb-2" style={{ fontVariationSettings: "'wdth' 125, 'wght' 600" }}>Competition Training</h1>
                 <p className="pb-6 text-gray-400">This training focuses on a wide variety of techniques, with a slant towards preparation for competition (if the health situation allows). All levels welcome!</p>
                 <p className="text-lg">Time and Location TBD</p>
               </div>
-              <div className="border-solid py-6 pl-6 pr-8" style={{ borderWidth: "1px" }}>
-                <h1 className="text-xl pb-2" style={{ fontVariationSettings: "'wdth' 125, 'wght' 600" }}>Conditioning Training</h1>
-                <p className="pb-6 text-gray-400">
-                  This fully virtual class will help you build strength and stamina through conditioning exercises.
-                  No forms or techniques are taught during this session.
-                </p>
-                <p className="text-lg">Time and Zoom Link TBD</p>
-              </div>
             </div>
             <p className="text-lg max-w-3xl pt-6">
-              As it currently stands, in order to support our training, UCSD Wushu will be hosting a combination of in-person and Zoom-based classes.
-              All in-person classes will also be livestreamed through Zoom, but no guided instruction will be provided through Zoom for these in-person classes.
+              Our training will be primarily in-person. More information on time and location will be provided after our GBM.
             </p>
             <h2 className="text-2xl pt-8 pb-4" style={{ fontVariationSettings: "'wdth' 125, 'wght' 700" }}>Safety guidelines</h2>
             <p className="text-lg max-w-3xl">
-              UCSD Recreation will be publishing comprehensive safety guidelines for facilitating safe outdoors recreational activities,
-              which will be published here when they are available. Regardless, we will also be abiding by the following rules:
+                In accordance with UCSD Recreation and local guidelines on COVID-19 safety, we will be observing the followingrules for all UCSD Wushu classes.
               <ul className="list-disc pt-2">
                 <li>All members will maintain at least six feet of distance.</li>
                 <li>No weapons will be shared between members.</li>
