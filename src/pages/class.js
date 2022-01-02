88f3f65c1163b8eeeb891280ca5abb314353caf6
David Cao <david@cao.sh>
2022-01-02 14:33:59 -0800

    Update class info

--- src/pages/class.js
+++ src/pages/class.js
@@ -25,27 +25,30 @@ const IndexPage = () => {
         <div className="border-solid lg:border-l min-w-full min-h-screen">
           <BackgroundImage className="px-8 lg:px-16 pt-40 lg:pt-16 pb-8 lg:pb-16 border-solid border-b bg-tint" fluid={data.masa20.childImageSharp.fluid}>
             <h1 className="text-3xl lg:text-4xl mt-auto lg:pr-64 z-10" style={{ fontVariationSettings: "'wdth' 125, 'wght' 700" }}>
-              Return to Wushu
+              Class
             </h1>
-              <h3 className="text-lg lg:text-xl text-gray-500" >Last updated <strong>September 9, 2021</strong></h3>
+              <h3 className="text-lg lg:text-xl text-gray-500" >Last updated <strong>January 2, 2022</strong></h3>
           </BackgroundImage>
           <div className="px-8 lg:px-16 py-16">
             <p className="text-lg max-w-3xl">
-              For the 2021-22 school year, UCSD Wushu plans on offering wushu classes primarily in-person. All of our classes will be taught outdoors, and will feature intensive training for individuals of all skill levels, beginning and advanced alike. All classes feature technique drills, advanced technique training (e.g. flips) for advanced students, and form teaching and practice.
+              For the remainder of the 2021-22 school year, UCSD Wushu plans on offering wushu classes primarily in-person; however, this has been temporarily delayed due to the recent rise of COVID-19 cases. In the event that classes continue, all of our classes will be taught outdoors, and will feature intensive training for individuals of all skill levels, beginning and advanced alike. All classes feature technique drills, advanced technique training (e.g. flips) for advanced students, and form teaching and practice.
+            </p>
+            <p className="text-lg max-w-3xl">
+              More information will be provided on this page and through the usual channels (i.e. Discord announcements) as the quarter progresses.
             </p>
             <h2 className="text-2xl pt-8 pb-4" style={{ fontVariationSettings: "'wdth' 125, 'wght' 700" }}>Classes</h2>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-2 max-w-5xl">
               <div className="border-solid py-6 pl-6 pr-8" style={{ borderWidth: "1px" }}>
                 <h1 className="text-xl pb-2" style={{ fontVariationSettings: "'wdth' 125, 'wght' 600" }}>Sunday Open Training</h1>
-                <p className="pb-6 text-gray-400">This class is coached primarily by <strong>David</strong>, with <strong>Bobby</strong> assisting for advanced students.</p>
+                <p className="pb-6 text-gray-400">This class is coached by <strong>David</strong> and <strong>Bobby</strong>.</p>
                 <p className="pb-6 text-gray-400">Registration for this class is handled directly through UCSD Wushu. (In other words: you can just show up, and we can handle payment and membership later!)</p>
                   <p className="text-lg">Sunday 3-5p, <a class="border-b border-gray-700 hover:border-gray-100" href="https://goo.gl/maps/BQ5BSyPdMCShUUf8A">Marshall Field</a> | $20</p>
               </div>
               <div className="border-solid py-6 pl-6 pr-8" style={{ borderWidth: "1px" }}>
-                <h1 className="text-xl pb-2" style={{ fontVariationSettings: "'wdth' 125, 'wght' 600" }}>Tuesday Rec Training</h1>
-                <p className="pb-6 text-gray-400">This class is coached by <strong>Bobby</strong>.</p>
-                <p className="pb-6 text-gray-400">Participating in this class requires registering via the UCSD Recreation website (see below). As such, <strong>space is limited to 16 students total.</strong></p>
-                <p className="text-lg">Tuesday 10:30a-12:30p, Muir Field | <a href="https://rec.ucsd.edu/Program/GetProgramDetails?courseId=c347f814-b6d1-4135-9b1f-f75256df5a98&semesterId=79bf4c76-b403-4975-832f-17d27dfa1478" class="border-b border-gray-700 hover:border-gray-100">$56-70 thru rec.ucsd.edu</a></p>
+                <h1 className="text-xl pb-2" style={{ fontVariationSettings: "'wdth' 125, 'wght' 600" }}>Rec Training</h1>
+                <p className="pb-6 text-gray-400">This class is coached by <strong>David</strong>.</p>
+                <p className="pb-6 text-gray-400">Participating in this class requires registering via the UCSD Recreation website (see below). As such, <strong>space is limited.</strong></p>
+                <p className="text-lg">Location and Time TBD</p>
               </div>
             </div>
             <p className="text-lg max-w-3xl pt-6">
