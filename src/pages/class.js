85d78bb5d2aa515133607d1211faaab674901836
David Cao <david@cao.sh>
2022-01-10 20:42:29 -0800

    upd

--- src/pages/class.js
+++ src/pages/class.js
@@ -31,7 +31,7 @@ const IndexPage = () => {
           </BackgroundImage>
           <div className="px-8 lg:px-16 py-16">
             <p className="text-lg max-w-3xl">
-              For the remainder of the 2021-22 school year, UCSD Wushu plans on offering wushu classes primarily in-person; however, this has been temporarily delayed due to the recent rise of COVID-19 cases. In the event that classes continue, all of our classes will be taught outdoors, and will feature intensive training for individuals of all skill levels, beginning and advanced alike. All classes feature technique drills, advanced technique training (e.g. flips) for advanced students, and form teaching and practice.
+              For the remainder of the 2021-22 school year, UCSD Wushu plans on offering wushu classes primarily in-person. All of our classes will be taught outdoors, and will feature intensive training for individuals of all skill levels, beginning and advanced alike. All classes feature technique drills, advanced technique training (e.g. flips) for advanced students, and form teaching and practice.
             </p>
             <p className="text-lg max-w-3xl">
               More information will be provided on this page and through the usual channels (i.e. Discord announcements) as the quarter progresses.
@@ -40,15 +40,15 @@ const IndexPage = () => {
             <div className="grid grid-cols-1 md:grid-cols-2 gap-2 max-w-5xl">
               <div className="border-solid py-6 pl-6 pr-8" style={{ borderWidth: "1px" }}>
                 <h1 className="text-xl pb-2" style={{ fontVariationSettings: "'wdth' 125, 'wght' 600" }}>Sunday Open Training</h1>
-                <p className="pb-6 text-gray-400">This class is coached by <strong>David</strong> and <strong>Bobby</strong>.</p>
+                <p className="pb-6 text-gray-400">This class is coached by <strong>Bobby</strong> and <strong>David</strong>.</p>
                 <p className="pb-6 text-gray-400">Registration for this class is handled directly through UCSD Wushu. (In other words: you can just show up, and we can handle payment and membership later!)</p>
-                  <p className="text-lg">Sunday 3-5p, <a class="border-b border-gray-700 hover:border-gray-100" href="https://goo.gl/maps/BQ5BSyPdMCShUUf8A">Marshall Field</a> | $20</p>
+                  <p className="text-lg">Sunday 3-5p, Location TBD | $20</p>
               </div>
               <div className="border-solid py-6 pl-6 pr-8" style={{ borderWidth: "1px" }}>
                 <h1 className="text-xl pb-2" style={{ fontVariationSettings: "'wdth' 125, 'wght' 600" }}>Rec Training</h1>
                 <p className="pb-6 text-gray-400">This class is coached by <strong>David</strong>.</p>
                 <p className="pb-6 text-gray-400">Participating in this class requires registering via the UCSD Recreation website (see below). As such, <strong>space is limited.</strong></p>
-                <p className="text-lg">Location and Time TBD</p>
+                <p className="text-lg">Thursday 3-5p, Muir Field | <a href="https://rec.ucsd.edu/Program/GetProgramDetails?courseId=c347f814-b6d1-4135-9b1f-f75256df5a98&semesterId=9862f197-a38e-4d9d-bc31-348fd4f119ae">Register via rec.ucsd.edu</a></p>
               </div>
             </div>
             <p className="text-lg max-w-3xl pt-6">
