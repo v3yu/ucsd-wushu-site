407dbb2ec5d99db7eed71e27ce8a68a701b6047c
Jeremy Lei <jeremy.lei.2002@gmail.com>
2022-11-10 11:34:13 -0800

    Added intros, adjusted some cabinet member images

--- src/pages/cabinet.js
+++ src/pages/cabinet.js
@@ -8,7 +8,6 @@ import BackgroundImage from "gatsby-background-image"
 import { GatsbyImage } from "gatsby-plugin-image"
 
 const IndexPage = () => {
-  const austin_text="Austin is a Cognitive Science major in Warren College that is interested in expanding the Esports field. As a keen competitor, Austin loves to draw parallels between creating better competitors and better people. He thinks the secret to both is talk therapy with his collection of Sanrio plushie pals. Eternal rivals with fellow historian Jeremy Lei, Austin focuses on captivating the UCSD Wushu club with his video editing skills to match Jeremy's knack for video ideas. While some say they compromise to create unrivaled content, it's clear that Austin is the \"more talented and beautiful individual\" (endorsed by Jeremy Lei). On the other hand, Cindy and Austin work together to create a comfortable social community for the students of UCSD Wushu. With an ultimate goal to get the club to travel out to Head in the Clouds Festival over the Summer, Cindy and Austin are planning ahead by planning exciting events to increase the morale of the whole club."
 
   const data = useStaticQuery(graphql`
     query {
@@ -33,6 +32,26 @@ const IndexPage = () => {
         }
       }
       felicity: file(relativePath: { eq: "images/cabinet/felicity.jpg" }) {
+        childImageSharp {
+          gatsbyImageData(layout: CONSTRAINED, placeholder: TRACED_SVG, aspectRatio: 1.5)
+        }
+      }
+      austin: file(relativePath: { eq: "images/cabinet/austin.png" }) {
+        childImageSharp {
+          gatsbyImageData(layout: CONSTRAINED, placeholder: TRACED_SVG, aspectRatio: 1.5)
+        }
+      }
+      jeremy: file(relativePath: { eq: "images/cabinet/jeremy.jpg" }) {
+        childImageSharp {
+          gatsbyImageData(layout: CONSTRAINED, placeholder: TRACED_SVG, aspectRatio: 1.5)
+        }
+      }
+      nathan: file(relativePath: { eq: "images/cabinet/nathan.jpeg" }) {
+        childImageSharp {
+          gatsbyImageData(layout: CONSTRAINED, placeholder: TRACED_SVG, aspectRatio: 1.5)
+        }
+      }
+      zhoujie: file(relativePath: { eq: "images/cabinet/zhoujie.jpg" }) {
         childImageSharp {
           gatsbyImageData(layout: CONSTRAINED, placeholder: TRACED_SVG, aspectRatio: 1.5)
         }
@@ -65,7 +84,7 @@ const IndexPage = () => {
           <Member
             title="President"
             name="Felicity Le"
-            desc="Felicity Le is a fourth year Global Health major in Warren College. She joined wushu when she started college and has enjoyed it ever since. If she’s not at wushu, she can be found at dance practice! She has been dancing her whole life and could not imagine life without it. Along with this, Felicity can commonly be seen surrounded by green things and avocados - both loves of her life."
+            desc="Felicity is a fourth year Public Health, Epidemiology concentration major with a minor in Chinese Studies. She started wushu at UC San Diego and has loved the community and sport ever since. Besides wushu, Felicity loves to dance and has been dancing since she was 3 years old. She is currently on Ascension Competitive Dance Team at UC San Diego."
             img={data.felicity.childImageSharp.gatsbyImageData} />
           <Member
             title="Coach"
@@ -85,18 +104,19 @@ const IndexPage = () => {
           <Member
             title="Historian / Social Chair"
             name="Austin Soohoo"
-            desc={austin_text}
-            img={data.keerit.childImageSharp.gatsbyImageData} />
+            desc={`Austin is a Cognitive Science major in Warren College that is interested in expanding the Esports field. As a keen competitor, Austin loves to draw parallels between creating better competitors and better people. He thinks the secret to both is talk therapy with his collection of Sanrio plushie pals. Eternal rivals with fellow historian Jeremy Lei, Austin focuses on captivating the UCSD Wushu club with his video editing skills to match Jeremy's knack for video ideas. While some say they compromise to create unrivaled content, it's clear that Austin is the "more talented and beautiful individual" (endorsed by Jeremy Lei). On the other hand, Cindy and Austin work together to create a comfortable social community for the students of UCSD Wushu. With an ultimate goal to get the club to travel out to Head in the Clouds Festival over the Summer, Cindy and Austin are planning ahead by planning exciting events to increase the morale of the whole club.`}
+            img={data.austin.childImageSharp.gatsbyImageData}
+            useSmallFont="1" />
           <Member
             title="Demo Chair"
             name="Nathan Chao"
-            desc="Nathan is a second year computer engineering major"
-            img={data.keerit.childImageSharp.gatsbyImageData} />
+            desc={`Nathan is a second year computer engineering major in seventh college. He started practicing Shaolin when he was a wee little lad (Chaolin MonkTM). He would like everyone to know that he is, in fact, an aunt. An aunt that needs to stop doing the default Fortnite dance. Nathan can usually be found complaining about his programming assignments but not actually doing them; opting, instead, to teach other valuable members of UCSD Wushu how to do the wave (hi ben, wave!). He also derives a lot of joy out of yelling. just in general. "it's just cathartic y'know?" - Nathan at some point probably.`}
+            img={data.nathan.childImageSharp.gatsbyImageData} />
           <Member
             title="Treasurer"
             name="Zhoujie Yu"
-            desc="Zhoujie is a second year nano engineering major"
-            img={data.keerit.childImageSharp.gatsbyImageData} />
+            desc="Zhoujie Yu is second year Nanoengineering student at Warren college. She did a few years of wushu as a kid and is now getting back into it with UCSD Wushu. As the treasurer, she focuses on managing club funds and funding for competition. When not idling away time in the library with fellow wushu library residents, she can often be found consuming art or enjoying food. She also likes to try out different outdoor activities when not in hermit or studying mode."
+            img={data.zhoujie.childImageSharp.gatsbyImageData} />
           <Member
             title="Secretary"
             name="Serena Xie"
@@ -105,8 +125,8 @@ const IndexPage = () => {
           <Member
             title="Historian"
             name="Jeremy Lei"
-            desc="Jeremy is a third year mathematics-computer science major"
-            img={data.keerit.childImageSharp.gatsbyImageData} />
+            desc={`Jeremy “Big J” is a third year Math-CS major at seventh college and half of the Historian of the club! After a break from wushu, he joined in the club his 2nd year and has been an active and passionate member since. As of this moment, he’s in the midst of a poster buying spree (do not show him any sales of waifu posters). His goal for this year is to produce content involving the awesome events/shenanigans for Wushu! Beyond that, perhaps he’ll finally pick up another form…….`}
+            img={data.jeremy.childImageSharp.gatsbyImageData} />
           <Member
             title="Social Chair"
             name="Cindy Xu"
