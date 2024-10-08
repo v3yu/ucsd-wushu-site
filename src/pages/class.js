ada79accf2b1934358a7ed9dc69cc2b8c56893c3
Vivian Yu <146399020+v3yu@users.noreply.github.com>
2024-10-08 20:50:36 +0000

    updated class page

--- src/pages/class.js
+++ src/pages/class.js
@@ -41,12 +41,11 @@ const IndexPage = () => {
             <h1 className="text-3xl lg:text-4xl mt-auto lg:pr-64 z-10" style={{ fontVariationSettings: "'wdth' 125, 'wght' 700" }}>
               Class
             </h1>
-              <h3 className="text-lg lg:text-xl text-gray-500" >Last updated <strong>July 30, 2024</strong></h3>
+              <h3 className="text-lg lg:text-xl text-gray-500" >Last updated <strong>October 8 2024</strong></h3>
           </BackgroundImage>
           
           <div className="px-8 lg:px-16 py-16">
             <p className="text-lg max-w-3xl">
-            <br></br><strong>There are no official classes during the summer.</strong> Keep an eye out on our Discord server for informal practices hosted by members! 
             <br></br><br></br>Our classes feature intensive training for individuals of all skill levels. These practices feature technique drills, advanced technique training (e.g. flips), and form tutoring. 
             <br></br><br></br>For live updates regarding classes (especially because of weather), please join our <a className="cyan" href="https://discord.com/invite/F6gSrcG">Discord</a><br></br>
             </p>
@@ -55,30 +54,30 @@ const IndexPage = () => {
             </p>
             <h2 className="text-2xl pt-8 pb-4" style={{ fontVariationSettings: "'wdth' 125, 'wght' 700" }}>Classes</h2>
              {<div className="grid grid-cols-1 md:grid-cols-2 gap-2 max-w-5xl">
-              
+              {/*sunday class*/}
               <div className="border-solid py-6 pl-6 pr-8" style={{ borderWidth: "1px" }}>
-                <h1 className="text-xl pb-2" style={{ fontVariationSettings: "'wdth' 125, 'wght' 600" }}>Sunday Open Training</h1>
-                <p className="pb-6 text-gray-400">This class is coached by <strong><Link to="/cabinet" className="border-b border-gray-700 hover:border-gray-100">TBD</Link></strong>.</p>
+                <h1 className="text-xl pb-2" style={{ fontVariationSettings: "'wdth' 125, 'wght' 600" }}>Sunday</h1>
+                <p className="pb-6 text-gray-400">This class is coached by <strong><Link to="/cabinet" className="border-b border-gray-700 hover:border-gray-100">Nathan</Link></strong>.</p>
                 <p className="pb-6 text-gray-400">A class for everyone</p>
-                <p className="text-lg">TBD</p>
+                <p className="text-lg">1pm-3pm</p>
               </div>
               <BackgroundImage className="border-solid py-6 pl-6 pr-8" style={{ borderWidth: "1px" }} fluid={data.track.childImageSharp.fluid}>
                 <h1 className="text-xl pb-2" style={{ fontVariationSettings: "'wdth' 125, 'wght' 600" }}></h1>
               </BackgroundImage>
               <div className="border-solid py-6 pl-6 pr-8" style={{ borderWidth: "1px" }}>
-                <h1 className="text-xl pb-2" style={{ fontVariationSettings: "'wdth' 125, 'wght' 600" }}>TBD</h1>
+                <h1 className="text-xl pb-2" style={{ fontVariationSettings: "'wdth' 125, 'wght' 600" }}>Wednesday</h1>
                 <p className="pb-6 text-gray-400">This class is coached by <strong><Link to="/cabinet" className="border-b border-gray-700 hover:border-gray-100">TBD</Link></strong>.</p>
                 <p className="pb-6 text-gray-400">We focus on improving the quality of your combos, basics, and forms </p>
-                <p className="text-lg">TBD</p>
+                <p className="text-lg">3pm-5pm</p>
               </div>
               <BackgroundImage className="border-solid py-6 pl-6 pr-8" style={{ borderWidth: "1px" }} fluid={data.rimac.childImageSharp.fluid}>
                 <h1 className="text-xl pb-2" style={{ fontVariationSettings: "'wdth' 125, 'wght' 600" }}></h1>
               </BackgroundImage>
               <div className="border-solid py-6 pl-6 pr-8" style={{ borderWidth: "1px" }}>
-                <h1 className="text-xl pb-2" style={{ fontVariationSettings: "'wdth' 125, 'wght' 600" }}>Saturday Jumps Practice</h1>
+                <h1 className="text-xl pb-2" style={{ fontVariationSettings: "'wdth' 125, 'wght' 600" }}>Thursday</h1>
                 <p className="pb-6 text-gray-400">This class is coached by <strong><Link to="/cabinet" className="border-b border-gray-700 hover:border-gray-100">TBD</Link></strong></p>
-                <p className="pb-6 text-gray-400">This class is for working on all things jumps. We practice at a carpeted external studio, so check Discord to meetup for rides</p>
-                <p className="text-lg">TBD</p>
+                <p className="pb-6 text-gray-400">We focus on improving the quality of your combos, basics, and forms</p>
+                <p className="text-lg">5pm-7pm</p>
               </div>
               <BackgroundImage className="border-solid py-6 pl-6 pr-8" style={{ borderWidth: "1px" }} fluid={data.gd.childImageSharp.fluid}>
                 <h1 className="text-xl pb-2" style={{ fontVariationSettings: "'wdth' 125, 'wght' 600" }}></h1>
@@ -86,8 +85,6 @@ const IndexPage = () => {
       </div>}
             <p className="text-lg max-w-5xl pt-6">
               All classes are arranged directly with the club.
-              <li>TBD</li>
-              <li>TBD</li>
             </p>
             <p className="text-lg max-w-5xl pt-3">
               We accept payment in the following forms:
